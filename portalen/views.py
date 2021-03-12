02af0eecfc47fd33263c1d187f054f40a58a20fc
Lucaso98 <lucas.fransson98@gmail.com>
2021-03-12 17:46:23 +0100

    Upload project

---
+++ portalen/views.py
@@ -0,0 +1,33 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+
+@login_required(login_url='')
+def portal_page(request):
+    return render(request, 'portalen/portalpage.html')
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Användarnamn ELLER Lösenord är inte korrekt')
+
+    return render(request, 'registration/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
