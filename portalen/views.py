c675fa30c7431b247dd11b58267e85b140b51bd3
Lucaso98 <lucas.fransson98@gmail.com>
2021-03-21 20:06:51 +0100

    Error message

--- portalen/views.py
+++ portalen/views.py
@@ -23,7 +23,7 @@ def login_page(request):
                 login(request, user)
                 return redirect('home')
             else:
-                messages.info(request, 'Användarnamn ELLER Lösenord är inte korrekt.')
+                messages.info(request, 'Användarnamn ELLER Lösenord är inkorrekt.')
     context = {}
     return render(request, 'registration/login.html', context)
 
