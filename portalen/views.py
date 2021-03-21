5701cbc9adcc746a3517dbf13b7e661a39cd8f90
Lucaso98 <lucas.fransson98@gmail.com>
2021-03-21 15:20:49 +0100

    Update views.py

--- portalen/views.py
+++ portalen/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 
 
 def login_page(request):
