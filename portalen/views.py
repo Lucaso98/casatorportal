5b9fe6bdc57a85b7009ff31765553745fcfd7f83
Lucaso98 <lucas.fransson98@gmail.com>
2021-03-21 16:01:42 +0100

    Update views.py

--- portalen/views.py
+++ portalen/views.py
@@ -1,13 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required,
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_page(request):
-   if request.method == 'POST':
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
             form = AuthenticationForm(request.POST)
             username = request.POST.get['username']
             password = request.POST.get['password']
@@ -18,13 +21,14 @@ def login_page(request):
                 auth_login(request, user)
                 return redirect('home')
 
-        else:
-            messages.error(request, 'Användarnamnet ELLER lösenorder är inkorrekt.')
-            return redirect('login')
-            
-    else:
-        form=AuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
+                else:
+                    messages.error(
+                        request, 'Användarnamnet ELLER lösenorder är inkorrekt.')
+                    return redirect('login')
+
+            else:
+                form = AuthenticationForm()
+            return render(request, 'registration/login.html', {'form': form})
 
 
 def logout_user(request):
