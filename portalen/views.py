2da75057ec153667741cc1daa9b2d60ba34bd187
Lucaso98 <lucas.fransson98@gmail.com>
2021-03-21 16:13:15 +0100

    Update views.py

--- portalen/views.py
+++ portalen/views.py
@@ -1,9 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required,
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+
+
+@login_required(login_url='login')
+def portal_page(request):
+    return render(request, 'portalen/portalpage.html')
 
 
 def login_page(request):
@@ -20,17 +23,11 @@ def login_page(request):
                 login(request, user)
                 return redirect('home')
             else:
-                messages.error(request, 'Användarnamnet ELLER lösenordet är inkorrekt.')
+                messages.info(request, 'Användarnamn ELLER Lösenord är inte korrekt')
 
-            context = {}
-            return render(request, 'registration/login.html', context)
+    return render(request, 'registration/login.html')
 
 
 def logout_user(request):
     logout(request)
     return redirect('login')
-
-
-@login_required(login_url='login')
-def portal_page(request):
-    return render(request, 'portalen/portalpage.html')
